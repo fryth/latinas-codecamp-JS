933ae7fac4ba086800254a21855dee046ee8d6e9
Raquel Betancourt <rbet.anima@gmail.com>
2021-04-21 17:52:27 -0500

    Se añadieron los archivos correspondientes a la Clase 01 de Javascript, incluyen cambios en el archivo Variables en el cual se pusieron todos los tipos de variables

---
+++ index.js
@@ -0,0 +1,23 @@
+function sum (value1, value2) {
+    console.log(value1 + value2);
+}
+
+sum(155,78);
+
+function substract (value1,value2) {
+    console.log(value1-value2);
+}
+
+substract(548,265);
+
+function multiply (value1,value2) {
+    console.log(value1*value2);
+}
+
+multiply(899,91);
+
+function divide (value1,value2) {
+    console.log(value1/value2);
+}
+
+divide(567,3);
---
+++ variables.js
@@ -0,0 +1,30 @@
+//STRING
+let joke = "Knock knock";
+let favoriteColor = "burgundy";
+let bestUni = "Universidad Nacional Autónoma de México";
+
+//NUMBER
+let birthYear = 1994;
+let covid = 19;
+let arms = 2;
+
+//BOOLEAN
+let amICrazy = true;
+let isThisAMeme = false;
+
+//UNDEFINED
+let bankSavings = undefined;
+
+//NULL
+let myWallet = null;
+
+//ARRAY
+let whatsInMyPurse = ["cellphone", "money", "medicine", "lotion", true];
+
+//OBJECT
+let malcolmInTheMiddle = {
+    seasons : 7,
+    favoriteCharacter: "Francis",
+    bestEpisode: "S02 E03 - Lois's Birthday",
+    topQuote: "Dewey, get out of there. That's not your family."
+}
