9188d006ff9ad2bf563432a92f89ab6cdaf29d0f
fryth <82243826+fryth@users.noreply.github.com>
2021-05-03 17:38:12 -0500

    Se actualizaron las operaciones
    Se escribieron las funciones como Declaración de Función, Expresión de Función y Función Flecha.

--- index.js
+++ index.js
@@ -1,23 +1,82 @@
+//SUMA
+
+//Declaración de Función
 function sum (value1, value2) {
     console.log(value1 + value2);
 }
+sum(160, 20);
 
-sum(155,78);
-
-function substract (value1,value2) {
-    console.log(value1-value2);
+//Expresión de Función
+let sumResult = function sum(value1, value2) {
+    console.log(value1 + value2);
 }
+sumResult(499, 49);
 
-substract(548,265);
-
-function multiply (value1,value2) {
-    console.log(value1*value2);
+//Función Flecha
+let sumResult2 = (value1, value2) => {
+    console.log(value1 + value2);
 }
+sumResult2(891, 88);
 
-multiply(899,91);
+//------------------------------------
+//RESTA
 
-function divide (value1,value2) {
-    console.log(value1/value2);
+//Declaración de Función
+function substraction (value1, value2) {
+    console.log(value1 - value2);
 }
+substraction(991, 48);
 
-divide(567,3);
+//Expresión de Función
+let substract01 = function substraction(value1, value2) {
+    console.log(value1 - value2);
+}
+substract01(547, 89);
+
+//Función Flecha
+let substract02 = (value1, value2) => {
+    console.log(value1 - value2);
+}
+substract02(784, 18);
+
+//------------------------------------
+//MULTIPLICACIÓN
+
+//Declaración de Función
+function multiply (value1, value2) {
+    console.log(value1 * value2);
+}
+multiply(349, 99);
+
+//Expresión de Función
+let multiplyResult = function multiply(value1, value2) {
+    console.log(value1 * value2);
+}
+multiplyResult(13, 47);
+
+//Función Flecha
+let multiplyResult2 = (value1, value2) => {
+    console.log(value1 * value2);
+}
+multiplyResult2(589, 33);
+
+//------------------------------------
+//DIVISIÓN
+
+//Declaración de Función
+function division (value1, value2) {
+    console.log(value1 / value2);
+}
+division(16,4);
+
+//Expresión de Función
+let divResult = function division(value1, value2) {
+    console.log(value1 / value2);
+}
+divResult(44,11);
+
+//Función Flecha
+let divResult2 = (value1, value2) => {
+    console.log(value1 / value2);
+}
+divResult2(100,5);
